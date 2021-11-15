8480ae29b64847b11d9a5a6c3fa641a97e43ac43
Damian Trinca <damian.trinca@agilethought.com>
2021-11-15 19:42:38 -0300

    multisuply

--- src/app/modules/supplies/create-edit-main-supply/create-edit-main-supply.component.ts
+++ src/app/modules/supplies/create-edit-main-supply/create-edit-main-supply.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-edit-main-supply',
   templateUrl: './create-edit-main-supply.component.html',
-  styleUrls: ['./create-edit-main-supply.component.scss']
+  styleUrls: ['./create-edit-main-supply.component.scss'],
 })
 export class CreateEditMainSupplyComponent implements OnInit {
-
   formGroup: FormGroup;
-  error = 'Not valid'
-  suplies: string[] = ['Etiqueta', 'Corcho', 'Vacio 500 ml'];
+  error = 'Not valid';
+  supliesOptions: string[] = ['Etiqueta', 'Corcho', 'Vacio 500 ml'];
   helpClicked = false;
+  SupliesForm: FormArray;
 
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.createForm();
-
-    this.formGroup.valueChanges.pipe(debounceTime(500)).subscribe(form => {
-      console.log({ form, formGroup: this.formGroup })
-    });
+    this.formGroup.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((p) => console.log(p));
   }
 
   createForm() {
@@ -32,19 +36,36 @@ export class CreateEditMainSupplyComponent implements OnInit {
       Description: [null, Validators.required],
       Size: [null, [Validators.required, , Validators.minLength(1)]],
       Unit: [null, [Validators.required]],
-      Suplies: [null, [Validators.required]],
+      Suplies: this.formBuilder.array([this.createNewSupply()]),
+      PartialProduct: [null],
     });
   }
 
   controls(controlName) {
-    return this.formGroup.get(controlName) as FormControl
+    return this.formGroup.get(controlName) as FormControl;
   }
 
-  onSubmit(post) {
-    console.log(post);
+  onSubmit() {
+    console.log(this.formGroup.value);
   }
 
   openHelp() {
     this.helpClicked = !this.helpClicked;
+  }
+
+  addSuply() {
+    let supliesArray = this.formGroup.get('Suplies') as FormArray;
+    supliesArray.push(this.createNewSupply());
+  }
+
+  deleteSuply(index: number) {
+    let supliesArray = this.formGroup.get('Suplies') as FormArray;
+    supliesArray.removeAt(index);
+  }
+
+  createNewSupply(): FormGroup {
+    return this.formBuilder.group({
+      SingleSupply: '',
+    });
   }
 }
--- src/app/modules/supplies/supplies.module.ts
+++ src/app/modules/supplies/supplies.module.ts
@@ -4,19 +4,24 @@ import { DamajuanasComponent } from './damajuanas/damajuanas.component';
 import { NgModule } from '@angular/core';
 import { SuppliesRoutingModule } from './supplies-routing.module';
 import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatSelectModule } from '@angular/material/select'
-import { MatInputModule } from '@angular/material/input'
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CreateEditMainSupplyComponent } from './create-edit-main-supply/create-edit-main-supply.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
-  declarations: [DamajuanasComponent, MainSupliesComponent, CreateEditMainSupplyComponent],
+  declarations: [
+    DamajuanasComponent,
+    MainSupliesComponent,
+    CreateEditMainSupplyComponent,
+  ],
   imports: [
     CommonModule,
     SuppliesRoutingModule,
@@ -29,7 +34,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatDividerModule,
     MatIconModule,
-    MatButtonModule
-  ]
+    MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
+  ],
 })
-export class SuppliesModule { }
+export class SuppliesModule {}
