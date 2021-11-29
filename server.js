25bb5739bc32af11c9ac79665c46c2774c3c958c
Damian Trinca <damian.trinca@agilethought.com>
2021-11-29 11:45:36 -0300

    update frontEnd folder

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "./dist/frontend"));
+app.use(express.static(__dirname + "./frontend"));
 
 app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"))
+  res.sendFile(path.join(__dirname + "/frontend/index.html"))
 );
 
 app.listen(process.env.PORT || 8080);
