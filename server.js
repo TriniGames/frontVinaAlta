b74bd7e88b804f4dbe9458737966742e84538d4e
Damian Trinca <damian.trinca@agilethought.com>
2021-11-29 11:16:39 -0300

    fix sendfile

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + "./dist/frontend"));
 
 app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname + "dist/frontend/index.html"))
+  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"))
 );
 
 app.listen(process.env.PORT || 8080);
